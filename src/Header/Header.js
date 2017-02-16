0688009c327833a1ffd56c3865a2a342b4d7bf24
Emile Mirth Fleming <emile.fleming@gmail.com>
2017-02-16 08:30:33 -0800

    change app name to shomi

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -6,7 +6,7 @@ import SidebarToggle from '../Sidebar/SidebarToggle'
 export default function Header(props) {
   return (
     <nav className="Header">
-      <div className="logo">OGTV</div>
+      <div className="logo">shomi</div>
       <SidebarToggle toggleSidebar={props.toggleSidebar} />
     </nav>
   )
