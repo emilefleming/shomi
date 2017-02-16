76150f91de17db19545b109ac992cf0ad85688d2
Emile Mirth Fleming <emile.fleming@gmail.com>
2017-02-16 12:34:52 -0800

    Fix users seed add set increment to max val

--- seeds/1_users.js
+++ seeds/1_users.js
@@ -2,5 +2,10 @@ exports.seed = function(knex) {
   return knex('users').del()
     .then(function () {
       return knex('users').insert([{id: 1}, {id: 2}, {id: 3}])
+    })
+    .then(() => {
+      return knex.raw(
+        "SELECT setval('users_id_seq', (SELECT MAX(id) FROM users));"
+      );
     });
 };
